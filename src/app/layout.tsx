66b0e19b015e7dcf8e77ffbbee9a7ea6f8f39a35
장신원 <swjang@taratps.com>
2023-07-29 17:39:28 +0900

    🎨 set up eslint & prettier
    typescript 관련 eslint 추가
    prettier 옵션 및 플러그인 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'synuns blog',
   description: '시넌쓰의 개발 블로그',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div>Hello synuns!</div>
-  )
+  return <div>Hello synuns!</div>;
 }
